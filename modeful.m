0a0be67470b6ebf946367d83ad53293d6bd5329e
Shakir Sofi <71615838+ShakirSofi@users.noreply.github.com>
2020-12-19 22:39:32 +0545

    Delete modeful.m

--- modeful.m
+++
@@ -1,8 +0,0 @@
-function ggf =modeful()
-% This is the sub program which is called from main program and itself it
-% calls the modesolvelful, calls solver for Full order  system
-global j  A1 B1 C1 D1 Vd_on vin
-u1=[vin;Vd_on];
-modesolvefull(A1,B1,C1, D1,u1)
-j=j+1;
-end
